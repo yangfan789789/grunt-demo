034b53abe2f6519140783b0bf154c300ebe6bc41
yangfan789789 <1062208122@qq.com>
2020-02-27 16:24:08 +0800

    yangfan

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,27 @@
+// module.exports = function (grunt) {
+//     grunt.initConfig({
+//       less: {
+//         compile: {
+//           files: { 'dist/compiled.css': 'css/*.less' }
+//         } 
+//       }
+//     });
+  
+//     grunt.loadNpmTasks('grunt-contrib-less');
+  
+//     grunt.registerTask('default', ['less:compile']);
+//   };
 module.exports = function (grunt) {
-    grunt.initConfig({
-      less: {
-        compile: {
-          files: { 'dist/compiled.css': 'css/*.less' }
-        } 
-      }
-    });
-  
-    grunt.loadNpmTasks('grunt-contrib-less');
-  
-    grunt.registerTask('default', ['less:compile']);
-  };
+  grunt.initConfig({
+    htmlhint: {
+      options: {
+        htmlhintrc: '.htmlhintrc'
+      },
+      src: ['*.html']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-htmlhint');
+
+  grunt.registerTask('default', ['htmlhint']);
+};
